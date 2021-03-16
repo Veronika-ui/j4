550be790dcc025bd5085ae68416612ee17f25498
Veronika <n.kanchura.dodo@gmail.com>
2021-03-16 23:14:17 +0400

    'jv'

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+
+        int amount = 3500;
+        price = 20;
+        int miles = amount / price;
+
+        return miles;
+
+
+    }
+}
